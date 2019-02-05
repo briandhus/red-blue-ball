7b2ad7d8b6b0c2bba136d52ef18c4c7920507d81
Brian Huston <hbriandrums@gmail.com>
2019-02-05 13:50:48 -0800

    Define state object. RenderBall method funcional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,58 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      // visited: false,
+      // ballColor: null,
+      userCookie: {
+        ballColor: null,
+        visits: null,
+        hashId: null,
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.alertCookie();
+    this.renderBall();
+    // docCookies.getItem(name)
+    // this.getHashId;
+  }
+
+  renderBall() {
+    const randNum = max => Math.floor(Math.random() * max);
+    const colorClasses = ['blue-ball', 'red-ball'];
+    
+
+    if (!this.state.userCookie.hashId) {
+      this.setState({ userCookie: { ballColor: colorClasses[randNum(colorClasses.length)] }});
+    }
+  }
+
+  getHashId() {
+    fetch(`/id`)
+      .then(response => response.json())
+      .then((cookie) => {
+        this.setState({ userCookie: cookie });
+      })
+      .catch(error => console.log(error));
+  }
+
+  getCookie() {
+    this.setState({
+      userCookie: this.browser.cookies.getAll({hashId: this.state.userId})
+    })
+  }
+
+  alertCookie() {
+    alert(document.cookie);
   }
 
   render() {
+    const { userCookie } = this.state;
     return (
-      <div class="container">
-        <div class="ball"></div>
+      <div className="container">
+        <div className={`${userCookie.ballColor} ball`}></div>
       </div>
     )
   }
