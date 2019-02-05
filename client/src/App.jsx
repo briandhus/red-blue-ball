da4ff1cd0facf0ba8e699439376961269a080b74
Brian Huston <hbriandrums@gmail.com>
2019-02-04 18:03:38 -0800

    Server connected to client, serving static html file

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="container">
+        <div class="ball"></div>
+      </div>
+    )
+  }
+}
+
+export default App;
