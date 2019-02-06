21d42eaea283bf66399318edbbe3c2a6468c78db
Brian Huston <hbriandrums@gmail.com>
2019-02-05 21:54:25 -0800

    Removes functionality to set cookies in server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
+const utils = require('./lib/hashUtils');
+
 const app = express();
 const port = 3030;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+// app.get('/id', (req, res) => {
+//   res.cookie('hash_id', 'laksdjfh');
+//   res.cookie('ball_color', 'red-ball');
+//   res.cookie('visits', '1');
+//   res.send();
+// })
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-
-
-
-
-// from server
-// if cookie is empty
-// assign a hash, assign a color, times visited = 1;
-// send back cookies
